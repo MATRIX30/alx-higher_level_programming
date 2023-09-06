add4bf0900fb9070f55b3f160211d217080a473f
Matrix30 <Matrixboy30@gmail.com>
2023-09-06 02:21:15 +0100

    fiskkk

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 64, -1):
-    print(chr(i).upper() if i % 2 == 0 else chr(i).lower(), end="")
+    print(f"{chr(i).upper() if i % 2 == 0 else chr(i).lower()}", end="")
