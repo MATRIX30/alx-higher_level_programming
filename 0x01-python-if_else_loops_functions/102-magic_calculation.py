1a98e9812b6d3557da9f03793941584066422422
Matrix30 <Matrixboy30@gmail.com>
2023-09-06 02:40:01 +0100

    fidk

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    from magic_calculation_102 import add, sub
     if a < b:
-        c = add(a, b)
-        for n in range(4, 6):
-            c = add(c, n)
-        return (c)
+        return c
+    elif c > b:
+        return a + b
     else:
-        return (sub(a, b))
+        return a * b - c
