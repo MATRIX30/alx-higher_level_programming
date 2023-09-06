1dfbbd66e77752f796b091aaf7094409bc51c46b
Matrix30 <Matrixboy30@gmail.com>
2023-09-06 03:35:16 +0100

    another:

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print(f"{i}{j}", end=" ")
-print("\n")
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
