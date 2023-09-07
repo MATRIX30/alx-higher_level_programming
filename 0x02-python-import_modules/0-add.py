f1f6d7edfe1a437659a1d8fc41eb66dafc3d5b8e
Matrix30 <Matrixboy30@gmail.com>
2023-09-07 20:01:42 +0100

    dh

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("%d + %d = %d" % (a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     # print(f"{a} + {b} = {add_0.add(a, b)}")
