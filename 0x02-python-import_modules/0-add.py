047ec23e94f1c97bedfdfe37b5229cc4220074ac
Matrix30 <Matrixboy30@gmail.com>
2023-09-07 19:44:15 +0100

    another fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# from add_0 import add
-import add_0
-a = 1
-b = 2
 if __name__ == "__main__":
-    # print("%d + %d = %d" % (a, b, add(a, b)))
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    from add_0 import add
+    a = 1
+    b = 2
+    print("%d + %d = %d" % (a, b, add(a, b)))
+    # print(f"{a} + {b} = {add_0.add(a, b)}")
