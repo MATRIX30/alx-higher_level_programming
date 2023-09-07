00f3c8404174d3dcb4cc1053e7aad25f41778e66
Matrix30 <Matrixboy30@gmail.com>
2023-09-07 21:13:43 +0100

    dkdk

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print(f"{a:d} + {b:d} = {add(a, b):d}")
-    print(f"{a:d} + {b:d} = {sub(a, b):d}")
-    print(f"{a:d} + {b:d} = {mul(a, b):d}")
-    print(f"{a:d} + {b:d} = {div(a, b):d}")
+    print(f"{a:d} - {b:d} = {sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {div(a, b):d}")
