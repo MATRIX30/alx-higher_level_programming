b3133696df8d3f9a95e2ba8b673f073553516848
Matrix30 <Matrixboy30@gmail.com>
2023-09-08 03:18:18 +0100

    dhek

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     from calculator_1 import add, sub, mul, div
-    arg_count = len(sys.argv)
-    a = int(sys.argv[1])
-    print(sys.argv[3])
-    b = int(sys.argv[3])
+    arg_count = len(argv)
+    a = int(argv[1])
+    b = int(argv[3])
     if arg_count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if (sys.argv[2] not in ["+", "-", "/", "*"]):
+    if (argv[2] not in ["+", "-", "/", "*"]):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     else:
-        if (sys.argv[2] == "+"):
+        if (argv[2] == "+"):
             result = add(a, b)
-        elif (sys.argv[2] == "-"):
+        elif (argv[2] == "-"):
             result = sub(a, b)
-        elif (sys.argv[2] == "*"):
+        elif (argv[2] == '*'):
             result = mul(a, b)
-        elif (sys.argv[2] == "/"):
+        elif (argv[2] == "/"):
             result = div(a, b)
-        print(f"{a:d} {sys.argv[2]} {b:d} = {result:d}")
+        print(f"{a:d} {argv[2]} {b:d} = {result:d}")
