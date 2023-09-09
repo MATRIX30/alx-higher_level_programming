c783b556dd4a423c74f20b40db9da8222e942b43
Matrix30 <Matrixboy30@gmail.com>
2023-09-09 08:21:26 +0100

    fix support for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
-        print("{:d}".format(item))
+    if len(my_list) != 0:
+        for item in my_list[-1::-1]:
+            print("{:d}".format(item))
