0b89ddb724b697e4545f3bef83403ed564305c4b
Matrix30 <Matrixboy30@gmail.com>
2023-09-11 11:10:14 +0100

    finish 3 again

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         pass
     else:
-        for item in my_list[::-1]:
+        for item in reversed(my_list):
             print("{:d}".format(item))
