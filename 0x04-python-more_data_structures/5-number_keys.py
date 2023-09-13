e1d3df1bf415383b3354d335e8be15b6c01620dd
Matrix30 <Matrixboy30@gmail.com>
2023-09-13 18:29:12 +0100

    5. Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    return(len(a_dictionary.keys()))
+    #return(len(a_dictionary.keys()))
+    sum = 0
+    for i in a_dictionary.keys():
+        sum += 1
+    return sum
