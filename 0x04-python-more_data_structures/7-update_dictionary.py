568565a2f43fa04f799846df3a805257fe1bda6e
Matrix30 <Matrixboy30@gmail.com>
2023-09-13 20:55:46 +0100

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,5 @@ def update_dictionary(a_dictionary, key, value):
         if key not in a_dictionary:
             a_dictionary[key] = value
         else:
-            a_dictionary[key] = value
+            a_dictionary.update({key:value})
         return a_dictionary
