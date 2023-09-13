c1ed65ef01fae3bf70d3d05d0003fb451631fd76
Matrix30 <Matrixboy30@gmail.com>
2023-09-13 20:03:47 +0100

    9. Multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    res = {}
+    if a_dictionary:
+        for key in a_dictionary:
+            res[key] = a_dictionary[key] * 2
+    return res
+            
