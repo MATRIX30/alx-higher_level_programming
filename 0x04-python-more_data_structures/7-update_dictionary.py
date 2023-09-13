a948249c073dca40860eff390239ea0287dc17c6
Matrix30 <Matrixboy30@gmail.com>
2023-09-13 21:00:47 +0100

    dkd

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
+    if a_dictionary and key and value:
         if key not in a_dictionary:
             a_dictionary[key] = value
         else:
