ee596b806dd574638c7367d00596f145a789142b
Matrix30 <Matrixboy30@gmail.com>
2023-09-13 17:29:07 +0100

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-def 
+def square_matrix_simple(matrix=[]):
+    res = []
+    if matrix:
+        for items in matrix:
+            res.append(list(map((lambda x: x*x), items)))
+    return res
