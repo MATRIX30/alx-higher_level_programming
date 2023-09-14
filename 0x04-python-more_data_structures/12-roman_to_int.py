67c77293b43f7a4eea8a285cf886110919873e00
Matrix30 <Matrixboy30@gmail.com>
2023-09-14 03:09:59 +0100

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,12 +13,7 @@ def roman_to_int(roman_string):
     }
     res = 0
     i = 0
-    while i + 1 < len(roman_string):
-        if len(roman_string) == 1:
-            return roman_to_int_dict[roman_string[i]]
-        if (roman_to_int_dict[roman_string[i]] < roman_to_int_dict[roman_string[i+1]]):
-            res -= roman_to_int_dict[roman_string[i]]
-        else:
-            res += roman_to_int_dict[roman_string[i]]
+    while i < len(roman_string):
+        res += roman_to_int_dict[roman_string[i]]
         i +=1
     return res
