d8839b5e46856ec71fde8dfaad317530e33d697d
Matrix30 <Matrixboy30@gmail.com>
2023-09-26 20:47:02 +0100

    finish task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """This is an empty Square class
+    """
+    def __init__(self, size):
+        """This is the initilization function
+        it has a private size member
+        Args:
+            size: size of square
+         """
+        self._size = size
