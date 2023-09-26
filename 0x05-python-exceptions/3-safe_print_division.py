5bb0efc1bfc9fb8e0948086086c3df9e58f00868
Matrix30 <Matrixboy30@gmail.com>
2023-09-26 01:12:27 +0100

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,8 @@ def safe_print_division(a: int, b: int)-> float:
     try:
         res = a / b
     except ZeroDivisionError as z:
-       
+        res = None
     finally:
-        print("{}".format(res))
+        print("Inside result: {}".format(res))
+        return res
         
