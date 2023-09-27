675d70833349d55cfef63314e6e394cc2d1c4442
Matrix30 <Matrixboy30@gmail.com>
2023-09-27 01:04:58 +0100

    finish task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,14 +11,9 @@ class Square:
         Args:
             size: size of square
          """
-        try:
-            if (not isinstance(size, int)):
-                raise TypeError
-            elif (size < 0):
-                raise ValueError
-            else:
-                self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
