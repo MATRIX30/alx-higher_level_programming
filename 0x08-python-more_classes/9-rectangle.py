0976403a758fd041fe5bf298d7e13906552925f9
Matrix30 <Matrixboy30@gmail.com>
2023-10-03 01:38:35 +0100

    9. A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,16 +10,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0) -> None:
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
