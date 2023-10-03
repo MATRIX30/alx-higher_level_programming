548c6ff7c8b607c21b8a26a84449485378db1360
Matrix30 <Matrixboy30@gmail.com>
2023-10-03 01:33:35 +0100

    9. A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -135,4 +135,4 @@ class Rectangle:
         Returns:
             Rectangle: a square representation of a rectangle
         """
-        return (cls(size, size))d
+        return (cls(size, size))
