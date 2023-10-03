ceab4e9d6ba35fff287b9246d38f3bcd07e388e5
Matrix30 <Matrixboy30@gmail.com>
2023-10-03 01:46:32 +0100

    9. A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0) -> None:
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
