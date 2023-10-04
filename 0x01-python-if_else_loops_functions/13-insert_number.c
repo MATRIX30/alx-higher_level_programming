3132c1623b5feae1bfcc084c2c6fd4d44c7894fb
Matrix30 <Matrixboy30@gmail.com>
2023-10-04 11:36:30 +0100

    finish interview task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * insert_node - function to insert into a sorted linked list
+ * @head: pointer to the head node of list
+ * @number: the number to insert to the list
+ * Return: pointer to the new node
+*/
+listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node;
+	listint_t *cur, *prev;
+
+	if (*head == NULL)
+	{
+		return (add_nodeint_end(head, number));
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = number;
+	new_node->next = NULL;
+
+	cur = *head;
+	prev = *head;
+	while (cur->next != NULL)
+	{
+		cur = cur->next;
+		if (cur->n >= number)
+		{
+			prev->next = new_node;
+			new_node->next = cur;
+			return (new_node);
+		}
+		if (cur->next != NULL)
+		{
+			prev = prev->next;
+		}
+
+
+	}
+	if (cur->n >= number)
+	{
+		prev->next = new_node;
+		new_node->next = cur;
+		return (new_node);
+	}
+	else
+	{
+		return (add_nodeint_end(&cur, number));
+	}
+}
