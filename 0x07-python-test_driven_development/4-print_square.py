d4b283664806d0322836fdb262ed7765a6701fb1
Matrix30 <Matrixboy30@gmail.com>
2023-10-05 15:04:18 +0100

    finish task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,14 +8,20 @@ def print_square(size):
     Args:
             size (int): the size of the square to print
     """
+    if size is None:
+        raise TypeError("print_square() missing 1 required positional argument: 'size'")
+    if size == float("inf"):
+        raise OverflowError("cannot convert float infinity to integer")
     if isinstance(size, float):
         if size < 0:
             raise TypeError("size must be an integer")
         size = int(size)
+    
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    
 
     for i in range(0, size):
         for j in range(size):
