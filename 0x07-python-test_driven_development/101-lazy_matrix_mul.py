190612ba472a0411e24c3b125717a2169f84a4a0
Matrix30 <Matrixboy30@gmail.com>
2023-10-07 03:04:21 +0100

    finish

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Module to handle matrix multiplication using numpy"""
-
-
+# 101-lazy_matrix_mul.py
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
-def matrix_mul(m_a, m_b):
-    """_summary_
-
+def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
     Args:
-            m_a (list): list of int/float
-            m_b (list): list of int/float
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
-    return np.matmul(m_a, m_b)
+
+    return (np.matmul(m_a, m_b))
---
+++ 0x07-python-test_driven_development/101py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to handle matrix multiplication using numpy"""
+
+
+import numpy as np
+
+
+def matrix_mul(m_a, m_b):
+    """function to multiply 2 matrices m_a and m_b
+
+    Args:
+            m_a (list): list of int/float
+            m_b (list): list of int/float
+    """
+    return np.matmul(m_a, m_b)
