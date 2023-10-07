838be3ffdbf17216e30ead89bdd26e5c34d705ab
Matrix30 <Matrixboy30@gmail.com>
2023-10-07 02:42:40 +0100

    finish

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the module to multiply 2 matrix"""
+
+
+def matrix_mul(m_a, m_b):
+	"""_summary_
+
+	Args:
+		m_a (int/float): matrix - list of list
+		m_b (int/float): matrix - list of list
+	"""
