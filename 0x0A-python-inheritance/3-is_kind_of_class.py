753f15c53c1e45fd2ea757a9b78bb514085d4bd5
Matrix30 <Matrixboy30@gmail.com>
2023-10-09 16:39:00 +0100

    finish task 2 is same class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module to verify if an object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to verify if  object is an instance of, or if the object
+    is an instance of a class that
+    inherited from, the specified class ; otherwise False
+
+    Args:
+            obj (_type_): object to test its class
+            a_class (class): class to test
+    """
+    
+    return (isinstance(obj, a_class) or issubclass(type(obj), a_class))
