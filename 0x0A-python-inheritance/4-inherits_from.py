a01b123573e7294e729087c78a7704117a98f452
Matrix30 <Matrixboy30@gmail.com>
2023-10-09 16:54:32 +0100

    finish 4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module to verify if object is an instance of a class that inherited
+(directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """function to verify if object is an instance of a class that inherited
+        (directly or indirectly) from the specified class;
+        otherwise False
+
+
+    Args:
+            obj (_type_): object to test its class
+            a_class (class): class to test
+    """
+
+    return issubclass(type(obj), a_class)
