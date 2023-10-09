ffb1950014f06c8c3b6d8f2829bd31835fc4675a
Matrix30 <Matrixboy30@gmail.com>
2023-10-09 20:47:49 +0100

    finish task 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Geometry class"""
+
+
+class BaseGeometry:
+    """This is the BaseGeometry class"""
+
+    def area(self):
+        """area instance method"""
+        raise Exception("area() is not implemented")
