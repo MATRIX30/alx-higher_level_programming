d3c86c084582ab2a4e936b89eb0ca0994e379b3d
Matrix30 <Matrixboy30@gmail.com>
2023-10-09 16:20:18 +0100

    finish task 2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module to verify if an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """function to verify if obj is an instance a_class
+
+    Args:
+            obj (_type_): object to test its class
+            a_class (class): class to test
+    """
+    return isinstance(obj, a_class)
