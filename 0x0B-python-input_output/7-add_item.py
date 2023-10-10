8bb22c87c761382978dd200a0198bdc8af2c88de
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 21:05:02 +0100

    finish task 7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""	script to add all arguments to as python list 
-	and then save them to a file
+import sys
+import json
+import os
+"""	script to add all arguments to as python list
+    and then save them to a file
 """
+
 """importing necesarry modules"""
-import sys, json, os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+# import importlib
+# save_to_json_file = importlib.import_module("5-save_to_json_file")
+# load_from_json_file = importlib.import_module("6-load_from_json_file")
 
 flag = 0
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 # check if file exist if not create one with 'w' mode
 if not os.path.exists("add_item.json"):
-    open("add_item.json", 'w').close()
+    open("add_item.json", "w").close()
     flag = 1
 
-#verify if file was not present in the first place and was just created
+# verify if file was not present in the first place and was just created
 if flag == 1:
     res = []
 else:
     res = list(load_from_json_file("add_item.json"))
 
 
-# add command line arguments to values read to python list 
+# add command line arguments to values read to python list
 for i in range(1, len(sys.argv)):
     res.append(sys.argv[i])
 
