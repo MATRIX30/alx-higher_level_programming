5bc73c5d4cd36b138e07b3b4c07d15b342b31077
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 20:48:27 +0100

    finish task 7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,28 @@
 	and then save them to a file
 """
 """importing necesarry modules"""
-import sys, json
+import sys, json, os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""load the content of the file to a res list"""
-res = list(load_from_json_file("add_item.json"))
+flag = 0
 
-# add command line arguments to values read
-for i in sys.argv:
-    res.append(i)
+
+# check if file exist if not create one with 'w' mode
+if not os.path.exists("add_item.json"):
+    open("add_item.json", 'w').close()
+    flag = 1
+
+#verify if file was not present in the first place and was just created
+if flag == 1:
+    res = []
+else:
+    res = list(load_from_json_file("add_item.json"))
+
+
+# add command line arguments to values read to python list 
+for i in range(1, len(sys.argv)):
+    res.append(sys.argv[i])
 
 # save the updated results to the file
 save_to_json_file(res, "add_item.json")
-open("kd",'')
