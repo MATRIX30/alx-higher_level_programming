0c99e3b0128756df1265d413129f4e935dd02e8d
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 12:01:48 +0100

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module for a function that reads from a file in utf-8 encoding"""
+
+
+def read_file(filename=""):
+    """method to read from file
+
+    Args:
+            filename (str, optional): filename to read from. Defaults to "".
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
+    print()
