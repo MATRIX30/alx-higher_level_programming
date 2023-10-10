c80892b6d2e7f4d5917e2c293395ccc4cfab3859
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 03:13:36 +0100

    finish task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to add an attribute to an object"""
+
+
+def add_attribute(obj, attrib_name, attrib_value):
+    """method to add an attribute
+
+    Args:
+            obj (_type_): object whose attribute is to be added
+            attrib_name (_type_): the attribute name
+            attrib_value (_type_): attribute value
+    Raises:
+            TypeError: if the attribute cannot be added
+    """
+
+    if not hasattr(obj, attrib_name):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attrib_name, attrib_value)
