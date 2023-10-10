3f0f57a674ec154146b72a3d3b05ee73edc7c163
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 22:26:10 +0100

    finish task 11: rogramming/0x0B-python-in

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for a student class"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method/ constructor method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method that retrieves a dictionary
+        representation of a Student
+        """
+        filter_res = {}
+        if attrs is None:
+            return vars(self)
+        for key in attrs:
+            if key in vars(self):
+                filter_res[key] = vars(self)[key]
+        return filter_res
+
+    def reload_from_json(self, json):
+        """method to replace all attributes of student instance"""
+        for key in json.keys():
+            self.key = json[key]
