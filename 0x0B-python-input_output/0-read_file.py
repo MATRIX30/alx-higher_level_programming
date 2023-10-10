7ec1cb48cb7c22982bbd9ccf322d937fbf14601e
Matrix30 <Matrixboy30@gmail.com>
2023-10-10 13:32:48 +0100

    finish task 0 Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,4 @@ def read_file(filename=""):
             filename (str, optional): filename to read from. Defaults to "".
     """
     with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
-        print()
+        print(f.read())
