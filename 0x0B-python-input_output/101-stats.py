8e5acee0628618e0b38ee015359f515b170ca1a8
Matrix30 <Matrixboy30@gmail.com>
2023-10-11 21:33:19 +0100

    kd

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -23,22 +23,25 @@ if __name__ == "__main__":
     file_size = 0
     st_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     counter = 0
-    code = 1
-    try:
-        for line in sys.stdin:
-            line_dic = line.split(" ")
-            if counter == 10:
-                print("File size: {}".format(file_size))
-                for key, value in st_code.items():
-                    print("{}: {}".format(key, value))
-                counter = 1
-            code = int(line_dic[-2])
-            if code in st_code:
-                st_code[code] += 1
-            file_size += int(line_dic[-1])
-            counter += 1
-    except (KeyboardInterrupt, Exception):
-        print("File size: {}".format(file_size))
-        for key, value in sorted(st_code.items()):
-            print("{}: {}".format(key, value))
-        raise
+    code = 0
+   
+    for line in sys.stdin:
+        line_dic = line.split(" ")
+        if counter  == 10:
+            print("File size: {}".format(file_size))
+            for key, value in st_code.items():
+                print("{}: {}".format(key, value))
+            counter = 1
+        counter += 1
+        try:
+          code = int(line_dic[-2])
+          if code in st_code:
+              st_code[code] += 1
+              file_size += int(line_dic[-1])
+          
+        except (KeyboardInterrupt, Exception):
+            print("File size: {}".format(file_size))
+            for key, value in sorted(st_code.items()):
+                print("{}: {}".format(key, value))
+                
+  
