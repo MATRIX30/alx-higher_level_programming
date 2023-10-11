702adf4461ac7fe4bac24f010b15d4d7fe6bf51a
Matrix30 <Matrixboy30@gmail.com>
2023-10-12 00:18:07 +0100

    lst commit 0m

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,42 +13,40 @@ prints those statistics since the beginning:
           for this status code
         * format: <status code>: <number>
         * status codes should be printed in ascending order
-
 """
 
 
-import sys
-
-
 def print_status():
-    '''
-        Printing the status of the request
-    '''
+    """
+    Printing the status of the request
+    """
+
+    import sys
+
     counter = 0
     size = 0
     file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
 
     for lne in sys.stdin:
         line = lne.split()
-        try:
-            size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except Exception:
-            continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
+                  
                     print("{}: {}".format(key, val))
             counter = 0
         counter += 1
     if counter < 9:
         print("File size: {}".format(size))
         for key, val in sorted(status_codes.items()):
-            if (val != 0):
+            if val != 0:
                 print("{}: {}".format(key, val))
 
 
