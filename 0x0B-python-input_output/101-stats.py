95096080495632bc944aed490e3dcf851445d462
Matrix30 <Matrixboy30@gmail.com>
2023-10-12 00:30:30 +0100

    new release

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,4 @@
-##!/usr/bin/python3
+#!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics:
 - Input format: <IP Address> - [<date>] "GET /projects/260
   HTTP/1.1" <status code> <file size>
@@ -13,66 +13,33 @@ prints those statistics since the beginning:
           for this status code
         * format: <status code>: <number>
         * status codes should be printed in ascending order
+
 """
 
 
-def print_status():
-    """
-    Printing the status of the request
-    """
-
+if __name__ == "__main__":
     import sys
 
-    counter = 0
-    size = 0
     file_size = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
+    st_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    counter = 0
+    code = 0
 
-
-
-if __name__ == "__main__":
-
-    print_status()
-
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-    def check_match(line):
-        '''Checks for regexp match in line.'''
+    for line in sys.stdin:
+        line_dic = line.split(" ")
+        if counter == 10:
+            print("File size: {}".format(file_size))
+            for key, value in st_code.items():
+                print("{}: {}".format(key, value))
+            counter = 1
+        counter += 1
         try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except:
-            pass
+            code = int(line_dic[-2])
+            if code in st_code:
+                st_code[code] += 1
+                file_size += int(line_dic[-1])
 
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
-
+        except (KeyboardInterrupt, Exception):
+            print("File size: {}".format(file_size))
+            for key, value in sorted(st_code.items()):
+                print("{}: {}".format(key, value))
