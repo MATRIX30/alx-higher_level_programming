6e28ea2889d78c9fd86546e1302d6cdfc894c56b
Matrix30 <Matrixboy30@gmail.com>
2023-10-21 03:05:57 +0100

    finish

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""test module for rectangle class"""
-
+""" Module for test Rectangle class """
+import unittest
+import io
+import contextlib
+from io import StringIO
+from unittest import TestCase
 from models.rectangle import Rectangle
 from models.base import Base
-import unittest
+
 
 from unittest.mock import patch, Mock
 import io
