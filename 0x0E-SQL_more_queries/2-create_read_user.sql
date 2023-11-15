b31415b6e806a656f54eb75c0ade4cc9e69e4e21
Matrix30 <Matrixboy30@gmail.com>
2023-11-15 21:51:37 +0100

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -9,11 +9,11 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Creating user
-CREATE USER IF NOT EXISTS 'user_0d_2'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Granting user privileges
 GRANT SELECT 
 ON hbtn_0d_2.*
-TO 'user_0d_2'
+TO 'user_0d_2'@'localhost'
 WITH GRANT OPTION;
