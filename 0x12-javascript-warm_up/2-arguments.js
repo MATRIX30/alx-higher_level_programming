85eeb369dcdf9ad3bc8a723229bd7c2f08189e2b
Matrix30 <Matrixboy30@gmail.com>
2023-12-11 22:29:10 +0100

    finish task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // process command line arguments
+// const arg1 = process.argv[2]
+// const arg2 = process.argv[3]
 
-const arg1 = process.argv[2]
-const arg2 = process.argv[3]
-if (arg2) {
+const arg_count = process.argv.length
+if (arg_count >= 4) {
   console.log('Arguments found')
-} else if (arg1) {
+} else if (arg_count === 3) {
   console.log('Argument found')
 } else {
   console.log('No argument')
