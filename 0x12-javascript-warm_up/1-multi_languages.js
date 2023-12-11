e459f709e7ec16d3a72297b617ef92e81f4b7f8e
Matrix30 <Matrixboy30@gmail.com>
2023-12-11 16:25:01 +0100

    finish task  1. 3 languages1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// print top languages
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+console.log(c);
+console.log(python);
+console.log(javascript);
