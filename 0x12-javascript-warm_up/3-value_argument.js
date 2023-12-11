334d2e1950ae8e723833f1b7b95355c268f72dbb
Matrix30 <Matrixboy30@gmail.com>
2023-12-11 22:34:34 +0100

    fix semi-colon

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,9 @@
 // prints  1st value passed to it
 // else prints No argument
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 if (!arg) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
-  console.log(arg)
+  console.log(arg);
 }
