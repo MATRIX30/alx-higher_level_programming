3f64644270ac2a23ffb85a798570f54a27c1b5e1
Matrix30 <Matrixboy30@gmail.com>
2023-12-11 22:32:43 +0100

    fix semi colon

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // process command line arguments
-// const arg1 = process.argv[2]
-// const arg2 = process.argv[3]
 
-const arg_count = process.argv.length
-if (arg_count >= 4) {
-  console.log('Arguments found')
-} else if (arg_count === 3) {
-  console.log('Argument found')
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+if (arg2) {
+  console.log('Arguments found');
+} else if (arg1) {
+  console.log('Argument found');
 } else {
-  console.log('No argument')
+  console.log('No argument');
 }
