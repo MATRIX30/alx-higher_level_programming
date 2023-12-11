fed20dcae76bd459de118143588b1cdf9a5843bb
Matrix30 <Matrixboy30@gmail.com>
2023-12-11 21:59:42 +0100

    finish task

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints  1st value passed to it
+// else prints No argument
+
+const arg = process.argv[2]
+if (!arg) {
+  console.log('No argument')
+} else {
+  console.log(arg)
+}
