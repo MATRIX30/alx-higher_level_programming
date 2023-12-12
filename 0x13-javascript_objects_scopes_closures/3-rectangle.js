c7c75c690b8d8be5d57c4d523bb9fe76710e8d61
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 23:19:13 +0100

    fix 1-rectangle'

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Rectangle module
+
+module.exports = class Rectangle {
+	// rectangle class
+  
+	constructor (w = -1, h = -1) {
+	  if ((w <= 0) || (h <= 0)) {
+		// Do nothing
+	  } else {
+		this.width = w;
+		this.height = h;
+	  }
+	};
+
+  function print(params) {
+    console.log("hello");
+  }
+  };
+  
