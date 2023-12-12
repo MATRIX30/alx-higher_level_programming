b5489aecd63d36c0e56ff5ff30f17c19553b0121
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 03:12:19 +0100

    fix 12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,10 +2,9 @@
 // Replaces values 12 to 89
 
 const myObject = {
-	type: 'object',
-	value: 12
+  type: 'object',
+  value: 12
   };
   console.log(myObject);
   myObject.value = 89;
   console.log(myObject);
-  
