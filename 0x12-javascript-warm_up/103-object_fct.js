ae34a11ebcab3a62eee60808281f38083c8c33e5
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 02:45:33 +0100

    fix cooment

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
+// comment
+
 const myObject = {
 	type: 'object',
 	value: 12
   };
   console.log(myObject);
+  
   myObject.incr = function () {
 	this.value++;
   };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
   
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
