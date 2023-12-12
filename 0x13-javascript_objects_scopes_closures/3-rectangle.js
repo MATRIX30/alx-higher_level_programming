433762fafe5662b22b6805efb9ab2abecfadb37d
Matrix30 <Matrixboy30@gmail.com>
2023-12-13 00:45:07 +0100

    finish task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
-// Create an instance method called print() 
+// Create an instance method called print()
 // that prints the rectangle using the character X
 
 module.exports = class Rectangle {
-	// rectangle class
-  
-	constructor (w = -1, h = -1) {
-	  if ((w <= 0) || (h <= 0)) {
-		// Do nothing
-	  } else {
-		this.width = w;
-		this.height = h;
-	  }
-	};
+  // rectangle class
 
-  print() {
-    console.log("hello");
+  constructor (w = -1, h = -1) {
+    if ((w <= 0) || (h <= 0)) {
+    // Do nothing
+    } else {
+      this.width = w;
+      this.height = h;
+    }
   }
-  };
-  
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+};
