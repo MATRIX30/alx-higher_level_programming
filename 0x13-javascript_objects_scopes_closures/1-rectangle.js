dbf1b17cc936f8c54ce872070b637dead71430c0
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 19:27:11 +0100

    create instance attributes widht and hieght with constructor for rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Rectangle module
+
+module.exports = class Rectangle {
+  // rectangle class
+  width;
+  height;
+
+  constructor (w, h){
+    this.width = w;
+	this.height = h;
+  }
+}
