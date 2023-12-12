bf519a247ca4dfda2f7f451281a57309e645a42f
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 02:54:56 +0100

    fix spaces

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,15 +2,15 @@
 // comment
 
 const myObject = {
-	type: 'object',
-	value: 12
+  type: 'object',
+  value: 12
   };
   console.log(myObject);
-  
+
   myObject.incr = function () {
-	this.value++;
+  this.value++;
   };
-  
+
   myObject.incr();
   console.log(myObject);
   myObject.incr();
