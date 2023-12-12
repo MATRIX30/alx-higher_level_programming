907da60deb7720409f58252b71c36fb6162977d8
Matrix30 <Matrixboy30@gmail.com>
2023-12-12 23:29:53 +0100

    fix comments in 2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 /*
 Write a class Rectangle that defines a rectangle:
-
 	You must use the class notation for defining your class
 	The constructor must take 2 arguments w and h
 	Initialize the instance attribute width with the value of w
@@ -10,6 +9,7 @@ Write a class Rectangle that defines a rectangle:
 */
 
 module.exports = class Rectangle {
+  // rectangle class
   constructor (w = -1, h = -1) {
     if ((w <= 0) || (h <= 0)) {
       // Do nothing
