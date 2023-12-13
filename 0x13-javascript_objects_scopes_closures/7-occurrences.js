c5128ff676f8c83b316861ced0710f74c6b382cc
Matrix30 <Matrixboy30@gmail.com>
2023-12-13 03:55:00 +0100

    finish task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,4 +3,3 @@ exports.nbOccurences = function (list, searchElement) {
   const filteredList = list.filter((currentElement) => currentElement === searchElement);
   return filteredList.length;
 };
-
