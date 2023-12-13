a9a7921d1f6ff440b5f1b0c5c420454231589e41
Matrix30 <Matrixboy30@gmail.com>
2023-12-13 03:51:31 +0100

    finish everython

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// returns the number of occurrences in a list.
+
+exports.nbOccurences = function (list, searchElement) {
+	let count = 0;
+
+	for (const element of list) {
+	  if (element === searchElement) {
+		count++;
+	  }
+	}
+
+	return count;
+};
+
