eeb9ec658f91dfc2e49b6d28d5c653da8e461c17
Matrix30 <Matrixboy30@gmail.com>
2023-12-16 22:15:27 +0100

    fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
-# print first state objects from the database
+"""
+Write a script that prints the first State object
+from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from
+model_state import Base, State
+Your script should connect to a MySQL server running on
+localhost at port 3306
+The state you display must be the first in states.id
+You are not allowed to fetch all states from the database
+before displaying the result
+The results must be displayed as they are in the example
+below
+If the table states is empty, print Nothing followed by a
+new line
+Your code should not be executed when imported
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy.engine.url import URL
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    )
 
-    # url
-    url = {'drivername': 'mysql+mysqldb',
-           'host': 'localhost',
-           'port': '3306',
-           'username': argv[1],
-           'password': argv[2],
-           'database': argv[3]}
-
-    c_url = URL(**url)
-
-    # create engine, metadata for stored objects
-    engine = create_engine(c_url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    # session
-    session = Session(engine)
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     try:
-        first = session.query(State).first()
-        print("{}: {}".format(first.id, first.name))
+        # prepare query
+        res = session.query(State).order_by(State.id).first()
+        print(str(res.id)+": "+str(res.name))
     except Exception:
-        print('Nothing')
+        pass
 
-    # close
+    # closing the session
     session.close()
