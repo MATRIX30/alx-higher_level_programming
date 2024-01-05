2fde9afa42db5f1b9be4709fa90ff27c45391f62
Matrix30 <Matrixboy30@gmail.com>
2024-01-05 17:47:43 +0100

    finish send header variable with value using curl to server

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that  takes in a URL as an argument, sends a GET request to the URL, with a header variable(key:value) and displays the body of the response
+curl -H "X-School-User-Id: 98" "$1"
