94653d736a4c5f7a0de748fd9700698ca13419ce
Matrix30 <Matrixboy30@gmail.com>
2024-01-05 18:15:45 +0100

    send file with curl using post method"

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -X POST -F "file=$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
