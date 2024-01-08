3603f16eb93cfaf1f3bbba508e4a61bef7f1fbc3
Matrix30 <Matrixboy30@gmail.com>
2024-01-08 03:57:53 +0100

    finish task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """
-sha + author commits history
+Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and
+not empty, display the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages other than
+requests and sys
 """
-
-
 if __name__ == '__main__':
     import requests
     from sys import argv
-
-    name = argv[2]
-    repo = argv[1]
-    params = {'per_page': 10}
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(name, repo), params=params)
-    r = r.json()
-    for arg in r:
-        print("{}: {}".format(arg.get('sha'),
-                              arg.get('commit').get('author').get('name')))
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")
