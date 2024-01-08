30c0a81645e36bef2be03010a631a7d9b03c326c
Matrix30 <Matrixboy30@gmail.com>
2024-01-08 03:54:57 +0100

    finish task 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and
+not empty, display the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages other than
+requests and sys
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    from requests.auth import HTTPBasicAuth
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
