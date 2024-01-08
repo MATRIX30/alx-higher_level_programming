16609adebce536854f05b783abd3a9ca4625fdf7
Matrix30 <Matrixboy30@gmail.com>
2024-01-08 01:57:06 +0100

    finish task 0 What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Write a Python script that fetches https://alx-intranet.hbtn.io/status
+
+# You must use the package urllib
+# You are not allowed to import any packages other than urllib
+# The body of the response must be displayed like the following
+# example (tabulation before -)
+# You must use a with statement
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+    data_decoded = data.decode('utf-8')
+
+    # display data
+    print("Body Response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data_decoded)
