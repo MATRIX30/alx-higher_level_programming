df51e7a8f70f45d8a6f8081c0eee27859ac1a984
Matrix30 <Matrixboy30@gmail.com>
2024-01-08 02:15:51 +0100

    finish task 0 What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-# Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
 
-# You must use the package urllib
-# You are not allowed to import any packages other than urllib
-# The body of the response must be displayed like the following
-# example (tabulation before -)
-# You must use a with statement
+You must use the package urllib
+You are not allowed to import any packages other than urllib
+The body of the response must be displayed like the following
+example (tabulation before -)
+You must use a with statement
+"""
 
 if __name__ == "__main__":
     import urllib.request
