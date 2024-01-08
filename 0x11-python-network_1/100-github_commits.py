480a75c3874b3710eae97c47fc4e80320cc22ccf
Matrix30 <Matrixboy30@gmail.com>
2024-01-08 03:04:04 +0100

    finish advance task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """
-lists the 10 most recent commits on a given GitHub repository.
+Write a Python script that takes 2 arguments in order to
+solve this challenge.
+
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests
+and sys
+You don’t need to check arguments passed to the script
+(number or type)
+Only 17% of applicants for a backend position at Holberton
+finished this task in less than 15 minutes.
 """
 import sys
 import requests
@@ -10,8 +21,8 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    request = requests.get(url)
+    commits = request.json()
     try:
         for i in range(10):
             print("{}: {}".format(
