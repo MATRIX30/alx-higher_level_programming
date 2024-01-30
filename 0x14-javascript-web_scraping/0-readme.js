8faeb0f1939f1614effd68a7240207509fb7feaf
Matrix30 <Matrixboy30@gmail.com>
2024-01-30 22:46:03 +0100

    fix semi-standard complaince

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
-/*
-Write a script that reads and prints the content of a file.
-	The first argument is the file path
-	The content of the file must be read in utf-8
-	If an error occurred during the reading, print the
-	error object
-*/
+/* Write a script that reads and prints the content of a file.
+The first argument is the file path
+The content of the file must be read in utf-8
+If an error occurred during the reading, print the
+error object */
 
 const fs = require('fs');
 const args = process.argv.slice(2);
-file_path = args[0];
+const filePath = args[0];
 
-//
-fs.readFile(file_path, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-    return
+    return;
   }
   console.log(data);
-})
+});
