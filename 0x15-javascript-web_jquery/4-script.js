ff71f2dd855e9ac499cb0aa7e8b8820830cbcb7b
Matrix30 <Matrixboy30@gmail.com>
2024-02-12 22:16:57 +0100

    finish task 4 toggle

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$("DIV#toggle_header").click(function(){
+	if ($("header").hasClass("red")){
+		$("header").addClass("green")
+		$("header").removeClass("red")
+	} else {
+		$("header").addClass("red")
+		$("header").removeClass("green")
+	}
+});
