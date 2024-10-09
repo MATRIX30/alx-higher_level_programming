87323c4333c23dd4ad07de91c0bfcf8c19b0bc78
Matrix30 <matrixboy30@gmail.com>
2024-10-09 17:10:39 +0100

    refressing javascript warmup task

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// code to print a constant myVar
-
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// print top languages
-
-const c = 'C is fun';
-const python = 'Python is cool';
-const javascript = 'JavaScript is amazing';
-console.log(c);
-console.log(python);
-console.log(javascript);
