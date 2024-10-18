fe220734864ea674937f374caab043d2311474d4
Matrix30 <matrixboy30@gmail.com>
2024-10-19 00:26:36 +0100

    finish task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // print message depending on # of arguments
+const { argv } = require('node:process');
+const argCount = argv.length - 2;
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
